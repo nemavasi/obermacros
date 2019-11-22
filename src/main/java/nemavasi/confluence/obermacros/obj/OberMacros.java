c3acb7ebbd7f62309cb9517a14852f4af3bd0336
dvshalygin <thatbook2@gmail.com>
2019-11-22 14:48:31 +0300

    работает

--- src/main/java/nemavasi/confluence/obermacros/obj/OberMacros.java
+++ src/main/java/nemavasi/confluence/obermacros/obj/OberMacros.java
@@ -28,7 +28,7 @@ public class OberMacros implements Macro {
     }
 
     @Override
-    public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
+    public String execute(Map<String, String> map, String body, ConversionContext conversionContext) throws MacroExecutionException {
 
         pageBuilderService.assembler().resources().requireWebResource("nemavasi.confluence.obermacros:ober-macro");
 
@@ -47,6 +47,24 @@ public class OberMacros implements Macro {
 
         String result = output.toString();
         result = result.replace("__ATLASSIAN_BASE_URL__", baseURL);
+
+        if (body == null || body.trim().length() == 0) {
+            body =  "MODULE test; \n" +
+            "IMPORT JS;\n" +
+            "BEGIN\n" +
+            "JS.alert(\"Hello, World!\")\n" +
+            "END test.\n"
+            ;
+        }
+        result = result.replace("__MY_CODE__", body);
+
+//        Integer width, height;
+//        try {
+//            width = Integer.valueOf(map.get("width"));
+//            height = Integer.valueOf(map.get("height"));
+//        } catch (NumberFormatException e) {
+//            throw new RuntimeException("wrong number!");
+//        }
 
 //        String output = "<div class =\"helloworld\">";
 //        output = output + "<div class = \"" + map.get("Color") + "\">";
