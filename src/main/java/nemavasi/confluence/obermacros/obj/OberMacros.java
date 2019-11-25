2304706a9958bc10637e979932bee36c9d76425f
dvshalygin <thatbook2@gmail.com>
2019-11-25 13:59:28 +0300

    todo

--- src/main/java/nemavasi/confluence/obermacros/obj/OberMacros.java
+++ src/main/java/nemavasi/confluence/obermacros/obj/OberMacros.java
@@ -34,6 +34,47 @@ public class OberMacros implements Macro {
 
         StringBuilder output = new StringBuilder();
         InputStream is = OberMacros.class.getClassLoader().getResourceAsStream("templates/ober.vm");
+        InputStream modMath = OberMacros.class.getClassLoader().getResourceAsStream("modules/Math.Mod");
+        InputStream modString = OberMacros.class.getClassLoader().getResourceAsStream("modules/Strings.Mod");
+        InputStream modLog = OberMacros.class.getClassLoader().getResourceAsStream("modules/Log.Mod");
+        InputStream modDraw = OberMacros.class.getClassLoader().getResourceAsStream("modules/Draw.Mod");
+
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(modMath))) {
+            String strCurrentLine;
+            while ((strCurrentLine = buffer.readLine()) != null) {
+                output.append(strCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(modString))) {
+            String strCurrentLine;
+            while ((strCurrentLine = buffer.readLine()) != null) {
+                output.append(strCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(modLog))) {
+            String strCurrentLine;
+            while ((strCurrentLine = buffer.readLine()) != null) {
+                output.append(strCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(modDraw))) {
+            String strCurrentLine;
+            while ((strCurrentLine = buffer.readLine()) != null) {
+                output.append(strCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try (BufferedReader buffer = new BufferedReader(new InputStreamReader(is))) {
             String strCurrentLine;
             while ((strCurrentLine = buffer.readLine()) != null) {
@@ -58,6 +99,7 @@ public class OberMacros implements Macro {
         }
         result = result.replace("__MY_CODE__", body);
 
+        result = modMath + "\n" + modString + "\n" + modLog + "\n" + modDraw + "\n" + result;
 //        Integer width, height;
 //        try {
 //            width = Integer.valueOf(map.get("width"));
